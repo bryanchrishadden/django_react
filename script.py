2b6879fdeb3df3df96a3fec53b70ae07d315632f
bryan.hadden <bryan.hadden@mhp.com>
2019-05-13 11:30:42 -0400

    init environment

---
+++ script.py
@@ -0,0 +1,12 @@
+import math
+import os
+import sys
+
+import requests
+
+# def greet(who_to_greet):
+#     greeting = "Hello, {}".format(who_to_greet)
+#     return greeting
+
+name = input("Your name is?")
+print("Hello,", name)
